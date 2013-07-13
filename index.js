50e49a8e6454668e7d826d06b14dfebdf2d7d386
Alan Gutierrez <alan@prettyrobots.com>
2013-07-13 19:00:49 -0400

    Import read/write lock from Strata.
    Closes #7.
    
    File                             Raw         Min         Zip     Min/Zip
    index.js                   674/00.66   416/00.41   264/00.26   214/00.21

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-module.exports = 1;
+function Sequester () {
+    this._queue = [[]]
+}
+
+Sequester.prototype.exclude = function (callback) {
+    var queue = this._queue
+    queue.push([ callback ])
+    queue.push([])
+    if (queue[0].length == 0) {
+        this.unlock()
+    }
+}
+
+Sequester.prototype.unlock = function (page) {
+    var queue = this._queue
+    queue[0].shift()
+    while (queue[0].length == 0 && queue.length != 1) {
+        queue.shift()
+        queue[0].forEach(function (callback) { callback() })
+    }
+}
+
+Sequester.prototype.share = function (callback) {
+    var queue = this._queue
+    queue[queue.length - 1].push(callback)
+    if (queue.length == 1) callback()
+}
+
+module.exports = Sequester
