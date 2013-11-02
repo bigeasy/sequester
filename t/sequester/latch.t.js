53f7037f1dc571ec4456d26db52969972e5bdfcd
Alan Gutierrez <alan@prettyrobots.com>
2013-11-02 21:14:42 +0000

    Pass parameters to shared functions via unlock.
    Now when `unlock` is called to release an exclusive lock, the parameters
    passes to `unlock` will be passed to the functions given to subsequent
    calls to `share`. Simplifies the implementation of latches.
    
    Closes #23.

--- t/sequester/latch.t.js
+++ t/sequester/latch.t.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 
-require('proof')(1, function (deepEqual) {
+require('proof')(2, function (deepEqual) {
     var Sequester = require('../..')
 
     var order = []
-    library(function () {
+    library(function (value) {
         order.push('second')
         deepEqual(order, [ 'first', 'second' ], 'latch')
+        deepEqual(value, 1, 'value')
     })
 
     function library (callback) {
         var sequester = new Sequester
         sequester.exclude(function () {})
         order.push('first')
-        sequester.share(function () {
-            callback()
+        sequester.share(function (value) {
+            callback(value)
         })
-        sequester.unlock()
+        sequester.unlock(1)
     }
 })
