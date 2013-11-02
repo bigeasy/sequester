affc4af9c744b47a55f61f3150437b86718344f3
Alan Gutierrez <alan@prettyrobots.com>
2013-11-02 21:14:42 +0000

    Pass parameters to shared functions via unlock.
    Now when `unlock` is called to release an exclusive lock, the parameters
    passes to `unlock` will be passed to the functions given to subsequent
    calls to `share`. Simplifies the implementation of latches.
    
    Closes #23.

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
+var __slice = [].slice
+
 function Sequester () {
     this._queue = [[]]
+    this._shared = []
 }
 
 Sequester.prototype.share = function (callback) {
     var queue = this._queue
     queue[queue.length - 1].push(callback)
-    if (queue.length == 1) callback()
+    if (queue.length == 1) callback.apply(null, this._shared)
 }
 
 Sequester.prototype.exclude = function (callback) {
     var queue = this._queue
     queue.push([ callback ])
     queue.push([])
-    if (queue[0].length == 0) {
-        this.unlock()
-    }
+    if (queue[0].length == 0) this.unlock()
 }
 
-Sequester.prototype.unlock = function (page) {
+Sequester.prototype.unlock = function () {
     var queue = this._queue
+    if (!(queue.length % 2)) {
+        this._shared = __slice.call(arguments)
+    }
     queue[0].shift()
     while (queue[0].length == 0 && queue.length != 1) {
         queue.shift()
         queue[0].slice().forEach(function (callback) {
-            callback()
-        })
+            callback.apply(null, this._shared)
+        }, this)
     }
 }
 
